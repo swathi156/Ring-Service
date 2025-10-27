56c2f57df1ff4fc9e800857f6e0bf84bcab23ce0
swathi156 <swathiprabhu016@gmail.com>
2025-10-27 11:02:44 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const db = require("./models");
+const ringIdRoutes = require("./routes/ringIdRoutes");
+const walletRoutes = require("./routes/walletRoutes");
+const walletHistoryRoutes = require("./routes/walletHistoryRoutes");
+
+const app = express();
+app.use(bodyParser.json());
+
+// Routes
+app.use("/ring", ringIdRoutes);
+app.use("/wallet", walletRoutes);
+app.use("/wallet-history", walletHistoryRoutes);
+
+// Sync DB & start server
+db.sequelize.sync({ alter: true }).then(() => {
+  console.log("Database synced");
+  app.listen(process.env.PORT || 5001, () =>
+    console.log("Server running on http://localhost:5001")
+  );
+}).catch(err => console.log(err));
