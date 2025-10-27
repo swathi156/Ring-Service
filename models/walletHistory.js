2b2a2d7a2d18eca88ece5796cb9ca44fea7daa13
swathi156 <swathiprabhu016@gmail.com>
2025-10-27 10:56:46 +0530

    Create walletHistory.js

---
+++ models/walletHistory.js
@@ -0,0 +1,16 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/db");
+const Wallet = require("./wallet");
+
+const WalletHistory = sequelize.define("WalletHistory", {
+  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+  device_id: { type: DataTypes.STRING, allowNull: false },
+  transactionAmount: { type: DataTypes.FLOAT, allowNull: false },
+  transactionType: { type: DataTypes.ENUM("credit", "debit"), allowNull: false },
+});
+
+Wallet.hasMany(WalletHistory, { foreignKey: "device_id", sourceKey: "device_id" });
+WalletHistory.belongsTo(Wallet, { foreignKey: "device_id", targetKey: "device_id" });
+
+
+module.exports = WalletHistory;
