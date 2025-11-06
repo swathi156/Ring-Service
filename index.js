1193d0d4195ef72a5e6b360f6b22a704492782b0
swathi156 <swathiprabhu016@gmail.com>
2025-11-06 13:51:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const db = require("./models");
 const ringIdRoutes = require("./routes/ringIdRoutes");
 const walletRoutes = require("./routes/walletRoutes");
 const walletHistoryRoutes = require("./routes/walletHistoryRoutes");
-
+const paymentRoutes = require("./routes/paymentRoutes");
+const reconciliationRoutes = require("./routes/reconciliationRoutes");
 const app = express();
 app.use(bodyParser.json());
 
@@ -12,10 +13,12 @@ app.use(bodyParser.json());
 app.use("/ring", ringIdRoutes);
 app.use("/wallet", walletRoutes);
 app.use("/wallet-history", walletHistoryRoutes);
+app.use("/payment", paymentRoutes);
 
+app.use("/reconciliation", reconciliationRoutes);
 // Sync DB & start server
 db.sequelize.sync({ alter: true }).then(() => {
-  console.log("Database synced");
+  console.log("Database synced ");
   app.listen(process.env.PORT || 5001, () =>
     console.log("Server running on http://localhost:5001")
   );
